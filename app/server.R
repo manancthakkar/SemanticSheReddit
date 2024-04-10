f6a8999fb531798d24d906a843950eb78751e154
manancthakkar <158368567+manancthakkar@users.noreply.github.com>
2024-04-10 15:58:59 -0400

    Update server.R

--- app/server.R
+++ app/server.R
@@ -2,11 +2,9 @@ library(shiny)
 library(wordcloud2)
 library(tm)
 library(wordcloud)
-library(syuzhet)  # For sentiment analysis
+library(syuzhet) 
 
-# Define server logic
 server <- function(input, output) {
-  # Reactive expression to fetch data based on subreddit selection
   data_selected <- reactive({
     switch(input$subreddit,
            "Feminism" = posts_women[["Feminism"]],
@@ -22,7 +20,7 @@ server <- function(input, output) {
            NULL)
   })
   
-  # Display summary statistics
+#summary section
   output$summaryOutput <- renderPrint({
     req(data_selected())
     data <- data_selected()
@@ -31,47 +29,38 @@ server <- function(input, output) {
           "\nAverage Number of Comments: ", mean(data$num_comments, na.rm = TRUE))
   })
   
-
+#wordcloud
   output$wordCloudOutput <- renderWordcloud2({
-    req(data_selected())
+    req(data_selected()) 
     data <- data_selected()
     
     if (is.null(data) || nrow(data) == 0) {
       return("No data to display.")
     }
     
-    # Assuming 'title' column has the relevant text and is a character vector
     text <- tolower(data$title) 
     
-    # Create a Corpus from the vector of text
     docs <- Corpus(VectorSource(text))
     
-    # Preprocess the text
     docs <- tm_map(docs, content_transformer(tolower))
     docs <- tm_map(docs, removePunctuation)
-    docs <- tm_map(docs, removeWords, stopwords("english"))  # Remove stopwords
+    docs <- tm_map(docs, removeWords, stopwords("english"))  
     
-    # Create a term-document matrix
     tdm <- TermDocumentMatrix(docs)
     
-    # Convert the matrix to a data frame
     tdm_df <- as.data.frame(as.matrix(tdm))
     
-    # Word frequencies data frame for wordcloud2
     word_freq_df <- data.frame(word = rownames(tdm_df), freq = rowSums(tdm_df, na.rm = TRUE))
     
-    # Order by frequency
     word_freq_df <- word_freq_df[order(word_freq_df$freq, decreasing = TRUE), ]
     
-    # Create the word cloud
     wordcloud(words = word_freq_df$word, freq = word_freq_df$freq, min.freq = 1,
               max.words = 200, random.order = FALSE, rot.per = 0.35, 
               colors = brewer.pal(8, "Set1"))
   })
   
   
-  
-  # Sentiment analysis plot
+#sentiment graph 
   output$sentimentPlot <- renderPlot({
     req(data_selected())
     data <- data_selected()
